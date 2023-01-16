7b65ccbb99dba8e2f5cd5256beb29ef6353a3601
Vman1907 <varshmaan.sonkar@gmail.com>
2023-01-16 18:49:47 +0530

    initialize files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
-import { Fragment } from 'react';
-import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
-import { ComingSoon } from './pages/ComingSoon/ComingSoon';
+import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router-dom";
+import { Teams } from './pages/Teams/Teams';
+import { Events } from './pages/Events/Events';
+import { Navbar } from './Components/Navbar';
+import { Footer } from './Components/Footer';
+import { Home } from './pages/Home/Home';
+import { Error } from './pages/404Error/404Error';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<ComingSoon />} />
-
-        <Route
-          path="*"
-          element={<Navigate to="/" replace />}
-        />
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="*" element={<Navigate to="/not-found" replace />} />
+        <Route path='/teams' element={<Teams />} />
+        <Route path='/events' element={<Events />} />
+        <Route path='/not-found' element={<Error />} />
+        <Route path="/events/hack-nita" element={<h1>Hackathon</h1>} />
+        <Route path="/events/tgoc" element={<h1>Hackathon1</h1>} />
+        <Route path="/events/ctf" element={<h1>Hackathon2</h1>} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
